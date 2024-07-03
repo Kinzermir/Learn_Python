d8d1ab31829956329bb2fef0cc1d59586077e129
Kinzermir <152514002+Kinzermir@users.noreply.github.com>
2024-07-03 23:27:56 +0500

    Modern-Python
    "Python is a versatile and readable language extensively used in web development, scientific computing, automation, and AI."

---
+++ Class_03/class3.py
@@ -0,0 +1,13 @@
+# we made this file to caught intelesenece.
+# https://www.w3schools.com/python/python_operators.asp from there we will study.
+# Operators:- 
+
+a : int = 2
+b : int = 3
+
+print(a+b) # additon
+print(a-b) # substraction
+print(a*b) # multiplication
+print(a/b) # division
+print(a%b) # modulus
+print(a**b)# exponential 
