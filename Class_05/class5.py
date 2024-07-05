040eabec1fcde7a5662a09db0927e432a9a1d188
Kinzermir <152514002+Kinzermir@users.noreply.github.com>
2024-07-05 23:14:58 +0500

    Learn Modern Python

---
+++ Class_05/class5.py
@@ -0,0 +1,8 @@
+data_base : list[tuple[str,str]] = [ # in list we can store tuple and tuple type is str and tuple denote by () brackets.
+    ("qasim","123"),
+    ("hanry","321"),
+    ("martin","986"),
+]
+
+for row in data_base :
+    print(row)
