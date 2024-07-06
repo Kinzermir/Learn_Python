97b69ea8ca97427ccce7502c137032c2aea50495
Kinzermir <152514002+Kinzermir@users.noreply.github.com>
2024-07-06 23:05:20 +0500

    Learn Modern Python

---
+++ Class_06/class6.py
@@ -0,0 +1,48 @@
+# from typing import Union  
+# numbers : Union[int, float] = 1.0
+# print(numbers)
+
+
+# from typing import Union  
+
+# per : Union[int, float] = int(input("Enter your percentage:"))
+# grade : Union[str, None] = None  
+# #           OR
+# # grade : str | None= None    
+
+# if per >= 80 :   
+#     grade = "A+"
+# elif per >= 70 :
+#     grade = "A"
+# elif per >= 60 :
+#     grade = "B"
+# elif per >= 50 :
+#     grade = "C" 
+# elif per >= 33 :
+#     grade = "E"
+# else :
+#     grade = "Fail"
+
+# print(f'Dear student your percentage is {per} now your calculated grade is : {grade} ')
+
+
+
+# from typing import Union  
+
+# per : Union[int, float] = int(input("Enter your percentage:"))
+# grade : Union[ None] = None     
+
+# if (per >= 0) and (per < 33) : # and meaning must dono condition poori hone chaye
+#     grade = "Fail"
+# elif (per >= 33) and (per < 50) : 
+#     grade = "D"
+# elif (per >= 50) and (per < 60) :
+#     grade = "c"
+# elif (per >= 60) and (per < 70) :
+#     grade = "B" 
+# elif (per >= 70) and (per < 80) :
+#     grade = "A"
+# elif (per >= 80) and (per <= 100) :
+#     grade = "A+"
+
+# print(f'Dear student your percentage is {per} now your calculated grade is : {grade} ')
